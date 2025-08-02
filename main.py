c3e53043791bb21bd03155d89aa611a1b17a00e5
Keshavgoyal14 <keshavgoyal1404@gmail.com>
2025-08-02 23:29:07 +0530

    change

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ document_cache = {}
 def get_document_hash(document_url: str) -> str:
     return hashlib.md5(document_url.encode()).hexdigest()
 
-@lru_cache(maxsize=32)
 def get_cached_vector_store(doc_hash: str):
     return document_cache.get(doc_hash)
 
